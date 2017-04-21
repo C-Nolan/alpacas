4ae7694a5c0a98e74e250ed52d6fca8b80f60ea8
sgunonu <rgunonu@gmail.com>
2017-04-20 23:51:27 -0500

    bug fix

--- app/overlay.js
+++ app/overlay.js
@@ -1,4 +1,5 @@
 window.onload=function(){
+	window.speechSynthesis.cancel();
      var test = document.getElementById("cbVision");
       test.addEventListener("click", function() { 
           addRemoveInput();
