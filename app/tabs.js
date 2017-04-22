39d09387edb89160c90afc9c763e48228c5ba1af
sgunonu <rgunonu@gmail.com>
2017-04-21 23:45:26 -0500

    text to speech works even if pop up is closed

---
+++ app/tabs.js
@@ -0,0 +1,13 @@
+// to load content_script all tabs
+function loadContentScriptInAllTabs() {
+    chrome.windows.getAll({'populate': true}, function(windows) {
+        for (var i = 0; i < windows.length; i++) {
+            var tabs = windows[i].tabs;
+            for (var j = 0; j < tabs.length; j++) {
+                chrome.tabs.executeScript(tabs[j].id, {
+                    file:'app/content_script.js', allFrames: true
+                });
+            }
+        }
+    });
+}
